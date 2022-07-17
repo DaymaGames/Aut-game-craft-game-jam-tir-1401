69f4d6d4aee4a14cfffae3b41466d5d8e960081b
bigofbig <hoseinstar80@gmail.com>
2022-07-17 16:02:43 +0430

    Update Boss1.cs

--- AUT Game Jam Project/Assets/Mohammad AI Virus/Boss 1/Boss1.cs
+++ AUT Game Jam Project/Assets/Mohammad AI Virus/Boss 1/Boss1.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class Boss1 : MonoBehaviour
 {
-    enum BossState {ShootAttack,CicleAttack,Rest }
+    enum BossState {ShootAttack,CicleAttack,Rest,Standby }
     [SerializeField]BossState bossState;
+    float shootingTimeLenght=2;
+
+    private void Update()
+    {
+        switch (bossState)
+        {
+            case BossState.ShootAttack:
+                //first shoot bullet for 10 sec
+                //after that switch to circle attack
+                print("shoot");
+                Invoke("SwitchToRest", shootingTimeLenght);
+                
+                break;
+            case BossState.CicleAttack:
+                //circle attack 
+                //switch to rest 
+                break;
+            case BossState.Rest:
+                //3 second rest
+                //after 3 second switch to shoot 
+                break;
+                
+
+        }
+    }
+     
+    void SwitchToRest()
+    {
+        bossState = BossState.Rest;
+    }
+
 }
