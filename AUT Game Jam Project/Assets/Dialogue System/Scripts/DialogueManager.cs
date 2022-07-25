2314d82f8d4815e9d2ad7498956cc6cb2c3114cb
hossein-hacker <t5bb4knj@gmail.com>
2022-07-25 21:44:43 +0430

    dialogues bug showing at the bottom of screen

--- AUT Game Jam Project/Assets/Dialogue System/Scripts/DialogueManager.cs
+++ AUT Game Jam Project/Assets/Dialogue System/Scripts/DialogueManager.cs
@@ -101,7 +101,7 @@ public class DialogueManager : MonoBehaviour
 
     public void EndDialogue()
     {
-        dialogueParent.DOMoveY(-dialogueParent.sizeDelta.y - 10, animationDuration)
+        dialogueParent.DOMoveY(-dialogueParent.sizeDelta.y - 50, animationDuration)
             .OnComplete(() => ShowingDialogue = false) ;
     }
 
@@ -111,5 +111,9 @@ public class DialogueManager : MonoBehaviour
         sentences = new Queue<string>();
         clips = new Queue<AudioClip>();
         names = new Queue<string>();
+
+        Vector2 pos = dialogueParent.position;
+        pos.y = -dialogueParent.sizeDelta.y - 50;
+        dialogueParent.position = pos;
     }
 }
