d8f597d167f407e45f1069b3c1af53281eda8e5c
hossein-hacker <t5bb4knj@gmail.com>
2022-07-25 18:21:10 +0430

    هیچی

--- AUT Game Jam Project/Assets/Prototype/RTLTextSetter.cs
+++ AUT Game Jam Project/Assets/Prototype/RTLTextSetter.cs
@@ -2,14 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RTLTMPro;
+using TMPro;
+using Sirenix.OdinInspector;
 
 public class RTLTextSetter : MonoBehaviour
 {
     public string text;
     public RTLTextMeshPro textMeshPro;
+    public float writeTime = 0.05f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetText();
+        }
+
+    }
+
     public void SetText()
     {
-        print(text);
-        textMeshPro.text += text;
+        StopAllCoroutines();
+        StartCoroutine(TypeText(writeTime));
+    }
+    IEnumerator TypeText(float writeTime)
+    {
+        foreach (var c in text.ToCharArray())
+        {
+            textMeshPro.text += c;
+
+            yield return new WaitForSeconds(writeTime);
+        }
     }
 }
